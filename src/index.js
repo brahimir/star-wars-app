b3e900a5af93f074b3c687f7be95214498ee2a68
brahimir <brahimir.nick@gmail.com>
2021-03-25 14:53:20 -0400

    Added Bootstrap imports.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+// Bootstrap
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
