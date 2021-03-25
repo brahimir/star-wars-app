8e48c7772dd1d32227852382bfdc9a02d21c6bff
brahimir <brahimir.nick@gmail.com>
2021-03-25 14:54:31 -0400

    Added routing to App.js.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from "./assets/media/logos/logo.svg";
 import "./assets/sass/App.scss";
-// Components
-import Nav from "./app/components/layout/Navigation";
-import Home from "./app/pages/Home";
-import List from "./app/pages/List";
-import About from "./app/pages/About";
 // Router
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// Components
+import Nav from "./app/components/layout/navigation/Navigation";
+import Home from "./app/pages/Home";
+import About from "./app/pages/About";
+import List from "./app/pages/List";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Route path="/home" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/list" component={List} />
-      </Router>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/home" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/list" exact component={List} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
