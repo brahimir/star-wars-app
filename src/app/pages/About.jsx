7ed33b12284bfe2c4d64da624493f77fc919b0c6
brahimir <brahimir.nick@gmail.com>
2021-03-28 21:21:00 -0400

    Added custom favicon, updated About page, updated button sizes.

--- src/app/pages/About.jsx
+++ src/app/pages/About.jsx
@@ -3,9 +3,8 @@ function Home() {
     <div className="container my-5">
       <h1 className="mb-5 text-warning">About</h1>
       <p>
-        This is a simple application demonstrating the fetching of character data from
-        <a href="https://swapi.dev/"> SWAPI</a>, and displaying additional information on individual
-        character pages.
+        This is a simple application that uses<a href="https://swapi.dev/"> SWAPI</a> to fetch Star Wars
+        characters and display additional information on individual character pages.
       </p>
     </div>
   );
